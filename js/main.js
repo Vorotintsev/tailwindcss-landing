6ce6a52a45c520a0a36baabc29a54053ad399895
Artem Vorotintsev <126006120+Vorotintsev@users.noreply.github.com>
2023-04-01 18:11:06 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,68 @@
+function toggleMenu() {
+    const burger = document.querySelector('#burger');
+    const menu = document.querySelector('#mobile-menu');
+    const body = document.querySelector('body');
+
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('active')
+        menu.classList.toggle('hidden')
+        menu.classList.toggle('flex')
+        body.classList.toggle('overflow-hidden')
+    })
+
+    window.addEventListener('resize', function() {
+        if(this.window.innerWidth > 767.99) {
+            menu.classList.add('hidden')
+            menu.classList.add('flex')
+            burger.classList.remove('active')
+            body.classList.remove('overflow-hidden')
+        }
+    })
+}
+
+toggleMenu()
+
+// Tabs
+function toggleTabs(tabsTriggerClass, tabsContentClass) {
+    const tabsTriggers = document.querySelectorAll(tabsTriggerClass);
+    const tabsContents = document.querySelectorAll(tabsContentClass);
+    tabsContents.forEach(content => {
+        content.classList.add('hidden')
+        tabsContents[0].classList.remove('hidden')
+    })
+    
+    tabsTriggers.forEach((trigger, idx) => {
+        trigger.addEventListener('click', () => {
+            tabsTriggers.forEach((t) => t.classList.remove('active'))
+
+            trigger.classList.add('active')
+
+            tabsContents.forEach(content => {
+                content.classList.add('hidden')
+            })
+
+            tabsContents[idx].classList.remove('hidden')
+        })
+    })
+}
+
+toggleTabs('.tab-trigger', '.tab-content')
+
+function accordion() {
+    const items = document.querySelectorAll('.accordion-trigger');
+
+    items.forEach(item => {
+        item.addEventListener('click', function() {
+            const  parent = item.parentNode
+
+            if(parent.classList.contains('active')) {
+                parent.classList.remove('active')
+            } else {
+                document.querySelectorAll('.accordion-item').forEach(child => child.classList.remove('active'));
+                parent.classList.add('active')
+            }
+        })
+    })
+}
+
+accordion()
